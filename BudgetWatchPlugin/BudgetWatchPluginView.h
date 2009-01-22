bcb1b4a113432a54c06a4128a37947f12f42be0f
Mario Aquino <mario.e.aquino@gmail.com>
2009-01-22 08:54:21 -0600

    Really added the Obj-C plugin plus Google Spreadsheet client code (oops last time)

---
+++ BudgetWatchPlugin/BudgetWatchPluginView.h
@@ -0,0 +1,16 @@
+//
+//  BudgetWatchPluginView.h
+//  BudgetWatchPlugin
+//
+//  Created by Mario Aquino on 1/22/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BudgetWatchPluginView : NSView <WebPlugInViewFactory>
+{
+}
+
+@end
